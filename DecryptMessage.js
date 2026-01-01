2ad7d9bd1f65e72cfb1961f0bdfd4fa66a502292
Barak <barakc121@gmail.com>
2026-01-01 12:46:46 +0200

    delete files

--- DecryptMessage.js
+++
@@ -1,6 +0,0 @@
-export async function deleteOrder(req, res) {
-  const user = parseInt(req.params.username, req.params.password);
-  if (isNaN(user)) return res.status(400).json({ error: "Invalid id" });
-
-
-}
